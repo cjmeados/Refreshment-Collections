060f2e5612542594fb3cebf3918fbd5cfe7152ea
cjmeados <meadoschris@gmail.com>
2025-12-19 20:28:48 -0600

    adds basic backend boilerplate

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const PORT = 5500;
 
 app.use(express.json());
 
+app.get('/test', (req, res) => {
+    res.json({message: 'Backend is working.'});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on https://localhost://${PORT}`);
+});
