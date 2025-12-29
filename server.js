c07296964bad03cf31490a7848f4f5cdf688c985
cjmeados <meadoschris@gmail.com>
2025-12-29 17:33:50 -0600

    fixes pathing for express in server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = 5500;
+const PORT = 3000;
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/test', (req, res) => {
     res.json({message: 'Backend is working.'});
