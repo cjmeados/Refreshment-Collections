9f14053a9a5017253517f403b50a34fae11a63a3
cjmeados <meadoschris@gmail.com>
2025-12-30 16:25:12 -0600

    implements a random drink api call parsed thru json and has been tested in postman

--- server.js
+++ server.js
@@ -15,8 +15,40 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-app.get('/api/drink/random', (req, res) => {
+app.get('/api/drink/random', async (req, res) => {
 
-    
+    const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+    try {
+        
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        const drink = data.drinks[0];
+
+        const ingredientsAndProportions = [];
+        for (let i = 1; i <= 15; i++) {
+            if (drink["strIngredient" + i]) {
+                ingredientsAndProportions.push({
+                    ingredient: drink["strIngredient" + i],
+                    measure: drink["strMeasure" + i] || ""
+                });
+            }
+        }
+
+        const payload = {
+            name: drink.strDrink,
+            alcoholic: drink.strAlcoholic,
+            glass: drink.strGlass,
+            instructions: drink.strInstructions,
+            thumbnail: drink.strDrinkThumb,
+            ingredientsAndProportions: ingredientsAndProportions
+        }
+
+        res.status(200).json(payload);
+
+    } catch (err) {
+        res.status(500).json({error: "Could not fetch drink."});
+    }
 
 });
